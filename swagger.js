dd27fa672e5180e5e9da206c9bf229143a767d3a
zacbe <zacbegonzalez@gmail.com>
2023-05-06 18:14:29 +0200

    chore: add swagger

---
+++ swagger.js
@@ -0,0 +1,20 @@
+/* eslint-disable node/no-unpublished-require */
+const swaggerAutogen = require("swagger-autogen")({ openapi: "3.0.0" });
+
+const doc = {
+  info: {
+    title: "My API",
+    description: "Description",
+  },
+  host: "localhost:3001",
+  schemes: ["http"],
+};
+
+const outputFile = "./swagger-output.json";
+const endpointsFiles = ["./src/app.js"];
+
+/* NOTE: if you use the express Router, you must pass in the 
+   'endpointsFiles' only the root file where the route starts,
+   such as index.js, app.js, routes.js, ... */
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
