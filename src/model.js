e7c3e58ab016b2b2b8449112545727c519a1f663
zacbe <zacbegonzalez@gmail.com>
2023-05-07 16:01:12 +0200

    chore: disable query logs

--- src/model.js
+++ src/model.js
@@ -3,6 +3,7 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize({
   dialect: "sqlite",
   storage: "./database.sqlite3",
+  logging: false,
 });
 
 class Profile extends Sequelize.Model {}
